de252aa9ccda22008c6c627b2b668fff1632de43
sallescosta <newcapital.in@gmail.com>
2024-12-06 20:12:07 +0100

    chore(CustomIcon): remove unused icon types and related imports

--- src/components/CustomIcon/Icon.tsx
+++ src/components/CustomIcon/Icon.tsx
@@ -1,37 +1,16 @@
 'use client'
 
 import { ComponentPropsWithoutRef } from 'react'
-import {
-  AudioLines,
-  Piano,
-  Waves,
-  MapPin,
-  Mail,
-  Github,
-  Linkedin,
-  Phone,
-} from 'lucide-react'
-import { openToast } from '@/helpers/openToaster/openToaster'
+
 import { Stack } from '@/components/Stack/Stack'
+import { Github, Linkedin, Mail, Phone } from 'lucide-react'
 
 export interface IconProps extends ComponentPropsWithoutRef<'svg'> {
-  icon:
-    | 'AudioLines'
-    | 'Piano'
-    | 'Waves'
-    | 'MapPin'
-    | 'Mail'
-    | 'Github'
-    | 'Linkedin'
-    | 'Phone'
+  icon: 'Mail' | 'Github' | 'Linkedin' | 'Phone'
 }
 
 export const CustomIcon = ({ icon, ...props }: IconProps) => {
   const Comp = {
-    AudioLines,
-    Piano,
-    Waves,
-    MapPin,
     Mail,
     Github,
     Linkedin,
@@ -44,9 +23,10 @@ export const CustomIcon = ({ icon, ...props }: IconProps) => {
 export const ClientWrapper = () => {
   return (
     <Stack className="gap-2">
-      <CustomIcon icon="Waves" strokeWidth={1.75} onClick={openToast} />
-      <CustomIcon icon="Piano" strokeWidth={1.75} onClick={openToast} />
-      <CustomIcon icon="AudioLines" strokeWidth={1.75} onClick={openToast} />
+      <CustomIcon icon="Mail" strokeWidth={1.75} />
+      <CustomIcon icon="Github" strokeWidth={1.75} />
+      <CustomIcon icon="Linkedin" strokeWidth={1.75} />
+      <CustomIcon icon="Phone" strokeWidth={1.75} />
     </Stack>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,40 +10,9 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        rescriptRed: '#e6484e',
-        azul: '#3f6cae',
-        azulFr: '#153854',
-        grayFr: '#545454',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
-        },
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
-        },
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
-        },
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
-        },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
-        },
+        primary: '#00A19DF2',
+        gray100: '#f8f8f8',
+        gray200: '#B0B0B0',
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',

efdc3a0ce5fc6b0cfe3af4033ad1ada86febee50
sallescosta <newcapital.in@gmail.com>
2024-12-11 16:53:24 +0100

    fix: cvv form validation

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -2,6 +2,7 @@ import { Stack } from '@/components/Stack/Stack'
 import { CheckoutForm } from '@/components/CheckoutForm/CheckoutForm'
 import { Summary } from '@/components/Summary/Summary'
 import { Suspense } from 'react'
+import { Loader } from '@/components/Loader/Loader'
 
 export default async function Checkout() {
   return (
@@ -9,7 +10,7 @@ export default async function Checkout() {
       as="main"
       className="relative min-h-[100dvh] overflow-y-auto bg-white md:flex md:flex-row md:justify-between"
     >
-      <Suspense fallback={<div>carregando...</div>}>
+      <Suspense fallback={<Loader isLoading={true} />}>
         <CheckoutForm />
         <Summary />
       </Suspense>
--- src/hooks/useFormValidation/formHelpers.ts
+++ src/hooks/useFormValidation/formHelpers.ts
@@ -1,9 +1,10 @@
 import { z } from 'zod'
 import { masks } from '@/hooks/useCredPrice/Mask'
+
 const monthValidationSchema = z.string().refine((value) => {
   const month = Number(value)
   return month > 0 && month <= 12
-}, 'Maximo 12')
+}, 'máximo 12')
 
 const minYear = new Date().getFullYear().toString().slice(-2)
 
@@ -11,46 +12,50 @@ const yearValidationSchema = (minYear: number) =>
   z.string().refine((value) => {
     const year = Number(value)
     return year > minYear
-  }, `Mínimo ${minYear}`)
+  }, `mínimo ${minYear}`)
 
 export const formSchema = z.object({
-  name: z.string().min(2, 'Nome deve ter pelo menos 2 caracteres'),
+  name: z.string().min(2, 'nome deve ter pelo menos 2 caracteres'),
   phone: z.string().refine((value) => /^\(\d{2}\) \d{4,5}-\d{4}$/.test(value), {
     message:
-      'O número de telefone deve estar no formato (XX) XXXX-XXXX ou (XX) XXXXX-XXXX.',
+      'o número de telefone deve estar no formato (xx) xxxx-xxxx ou (xx) xxxxx-xxxx',
   }),
   cpf: z
     .string()
-    .min(11, 'Mínimo 11 caracteres')
+    .min(11, 'mínimo 11 caracteres')
     .refine((value) => /^\d{3}\.\d{3}\.\d{3}-\d{2}$/.test(masks.cpf(value)), {
-      message: 'CPF inválido',
+      message: 'cpf inválido',
     }),
   email: z
     .string()
-    .min(1, { message: 'Formato inválido.' })
-    .email('Formato inválido.'),
+    .min(1, { message: 'formato inválido' })
+    .email('formato inválido'),
   cardNumber: z
     .string()
-    .min(16, 'Número de cartão inválido')
+    .min(16, 'número de cartão inválido')
     .refine((value) => /^[\d\s]+$/.test(masks.creditCard(value)), {
-      message: 'Número de cartão inválido',
+      message: 'número de cartão inválido',
     }),
   monthValidate: monthValidationSchema,
   yearValidate: yearValidationSchema(Number(minYear)),
-  cvv: z.string().min(3, 'CVV inválido'),
+  cvv: z
+    .string()
+    .min(3, 'mínimo 3 dígitos')
+    .max(4, 'máximo 4 dígitos')
+    .regex(/^\d+$/, 'cvv deve conter apenas números'),
   shares: z.enum([
-    '1x R$ 300',
-    '2x R$ 150',
-    '3x R$ 100',
-    '4x R$ 75',
-    '5x R$ 60',
-    '6x R$ 50',
-    '7x R$ 42,86',
-    '8x R$ 37,05',
-    '9x R$ 33,33',
-    '10x R$ 30',
-    '11x R$ 27,27',
-    '12x R$ 25',
+    '1x r$ 300',
+    '2x r$ 150',
+    '3x r$ 100',
+    '4x r$ 75',
+    '5x r$ 60',
+    '6x r$ 50',
+    '7x r$ 42,86',
+    '8x r$ 37,05',
+    '9x r$ 33,33',
+    '10x r$ 30',
+    '11x r$ 27,27',
+    '12x r$ 25',
     '',
   ]),
 })
